884869802142b9d5f8db9ffaef4cf36e39802913
poornasyamasundar <cs19b049@gmail.com>
2022-03-16 17:01:45 +0530

    enabled definition for multiple files

--- src/extension.ts
+++ src/extension.ts
@@ -1,58 +1,114 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { MessagePort, workerData } from 'worker_threads';
 
-let map = new Map();
+//This map contains the map between function names and function descriptions of the current file
+let functionDefinitionMap = new Map();
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	//call the function hello world to refetch the function definitions
+	//TODO: if the function definitions is changed then, how to update the map
 	console.log('Congratulations, your extension "sampleextension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('sampleextension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello Poorna');
+		getFunctionDefinitions();
 	});
 
 	context.subscriptions.push(disposable);
 
+	//This provides the hover.
 	vscode.languages.registerHoverProvider('python', {
 		provideHover(document, position, token) {
-			const range = document.getWordRangeAtPosition(position); 
-			const word = document.getText(range);
-			console.log("word from map is " + map.get(word));
-			if( map.has(word) )
+
+			const range = document.getWordRangeAtPosition(position); 		//fetch the range of the hovered word
+			const word = document.getText(range);							//fetch the word
+			if( functionDefinitionMap.has(word) )							//if the word has a definition, then return it
 			{
 				return {
-					contents: [map.get(word)],
+					contents: [functionDefinitionMap.get(word)],
 				};
 			}
-			else{
-				printDefinitionsForActiveEditor(document, position);
-			}
-			//console.log(document.eol);
-			//console.log(document.fileName);
-			//console.log(document.languageId);
-			//console.log(document.isClosed);
+			return {
+				contents: [],
+			};
 		}
 	});
 }
 
-async function printDefinitionsForActiveEditor(document: vscode.TextDocument, position: vscode.Position) {
+//This function populates the function definition map
+async function getFunctionDefinitions()
+{
+	//if no file openend, then return;
 	const activeEditor = vscode.window.activeTextEditor;
-	if (!activeEditor) {
+	if( !activeEditor )
+	{
 		return;
 	}
 
-	const definitions = await vscode.commands.executeCommand<vscode.Location[]>(
+	let document = activeEditor.document;
+	let u = document?.uri;
+	
+	//get the token legends
+	const tokensLegends = await vscode.commands.executeCommand<vscode.SemanticTokensLegend>(
+		'vscode.provideDocumentSemanticTokensLegend',
+		u,
+	);
+	
+	//For some reason the first time helloworld is called there is no output, so call again after 1 sec if no output
+	if( tokensLegends === undefined )
+	{
+		setTimeout(()=>
+		{vscode.commands.executeCommand(
+			'sampleextension.helloWorld',
+		);}, 1000);
+		return;
+	}
+
+	//fetch tokens
+	const tokens = await vscode.commands.executeCommand<vscode.SemanticTokens>(
+		'vscode.provideDocumentSemanticTokens',
+		u,
+	);
+
+	let currentLine = 0;
+	let charPos = 0;
+
+	//This for loop depends on the structure of the tokens returned, read the documentation to understand the below code.
+	for( let i = 0 ; i < tokens.data.length/5 ; i++ )
+	{
+		currentLine = currentLine + tokens.data[i*5];
+		if( tokens.data[i*5] === 0 )
+		{
+			charPos += tokens.data[i*5+1];
+		}
+		else
+		{
+			charPos = tokens.data[i*5+1];
+		}
+
+		let pos = new vscode.Position(currentLine, charPos);
+		let tokenName = document.getText(document.getWordRangeAtPosition(pos));
+		let tokenType = tokensLegends.tokenTypes[tokens.data[i*5+3]];				
+
+		//if the definition is not already added and if the token is a function 
+		if( !functionDefinitionMap.has(tokenName) && tokenType === 'function' )
+		{
+			let def = await getDefiniton(document, pos);		//fetch the definition
+			functionDefinitionMap.set(tokenName, def);
+		}
+	}
+	//print the function definition map after fetching all definitions
+	console.log("Succesfully fetched the definitions");
+	for( let [key,value] of functionDefinitionMap )
+	{
+		console.log(key, " - " , value);
+	}
+}
+
+async function getDefiniton(document: vscode.TextDocument, position: vscode.Position )
+{
+	//fetch all the definitions
+	const definitions = await vscode.commands.executeCommand<vscode.Location[]>
+	(
 		'vscode.executeDefinitionProvider',
 		document.uri,
 		position
@@ -60,51 +116,27 @@ async function printDefinitionsForActiveEditor(document: vscode.TextDocument, po
 
 	for (let definition of definitions) 
 	{
-		let sourceDef = document.getText(document.getWordRangeAtPosition(definition.range.start));
-		console.log("sourceDef is = " + sourceDef);
-		console.log(definition);
+		//get the source file( the file that contains the definition)
+		let sourceFile = await vscode.workspace.openTextDocument(definition.uri);
 		
-		const word = document.getText(document.getWordRangeAtPosition(position));
-		
-		if (map.has(word) === false) 
+		if( definition.range.start.line !== 0 )
 		{
-			map.set(word, "");
-			
-			let instances = await vscode.commands.executeCommand<vscode.Hover[]>(
-				'vscode.executeHoverProvider',
-				document.uri,
-				position
-			);
-			
-			for (let instance of instances) {
-				let markdown = instance.contents[0];
-				let str = "";
-				if ((markdown as vscode.MarkdownString).value) {
-					str = (markdown as vscode.MarkdownString).value;
-				}
-				if (str.substring(11, 19) === "function") {
-					console.log("line no is = " + definition.range.start.line);
-					if( definition.range.start.line !== 0 )
-					{
-						let description = document.lineAt(definition.range.start.line-1);
-						if( description.text[0] === '#' )
-						{
-							map.set(word, description.text.substring(1));
-						}
-						else
-						{
-							map.set(word, "No Definition");
-						}
-					}
-					else
-					{
-						map.set(word, "No Definition");
-					}
-				}
+			//get the description from the line above the declaration
+			let description = sourceFile.lineAt(definition.range.start.line-1);
+
+			//if it is comment, then it is the definition
+			if( description.text[0] === '#' )
+			{
+				return description.text.substring(1);
 			}
-		}
+			//else it is not the definition
+			else
+			{
+				return "No Definition Provided";
+			}
+		}		
 	}
+
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
